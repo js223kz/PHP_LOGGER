40f2d22250cba2487b9a9df7e12e215ba03c5f03
js223kz <js223kz@student.lnu.se>
2015-10-27 09:58:00 +0100

    HTML view IPlist

--- view/AdminView.php
+++ view/AdminView.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mkt
- * Date: 2015-10-22
- * Time: 14:10
- */
 
 namespace view;
-
 
 use model\LogFacade;
 require_once("model/LogItem.php");
@@ -21,10 +14,13 @@ class adminView
     private $logItems = array();
     private $ipaddresses = array();
 
+    public function getIPList(LogFacade $logFacade){
+        $this->setIPList($logFacade);
+    }
 
-    public function getIPList(LogFacade $logFacade)
+
+    private function setIPList(LogFacade $logFacade)
     {
-
         //returning array of LogItem objects
         $items = $logFacade->getLogAllItems();
 
@@ -39,16 +35,7 @@ class adminView
                 $this->getOccurances();
             }
         }
-
-        var_dump($this->logItems);
-    }
-
-    private function getOccurances(){
-        $count = array_count_values($this->ipaddresses);
-        for($i=0; $i < count($this->logItems); $i++){
-            $num = $count[$this->logItems[$i][Self::$ip]];
-            $this->logItems[$i][Self::$numberOfTimes] = $num;
-        }
+        $this->renderHTML();
     }
 
     private function checkIfIPUnique($item){
@@ -69,8 +56,48 @@ class adminView
             array_push($this->logItems, [Self::$ip => $item->m_ip, Self::$microTime => $item->m_microTime]);
         }
     }
+
+    private function getOccurances(){
+        $count = array_count_values($this->ipaddresses);
+        for($i=0; $i < count($this->logItems); $i++){
+            $num = $count[$this->logItems[$i][Self::$ip]];
+            $this->logItems[$i][Self::$numberOfTimes] = $num;
+        }
+    }
+
+    private function renderHTML(){
+        echo '<!DOCTYPE html>
+            <html>
+            <head>
+              <meta charset="utf-8">
+              <title>Login Example</title>
+            </head>
+            <body>
+              <h1>Logger</h1>
+              <div class="container">
+                ' .$this->renderIPList(). '
+              </div>
+             </body>
+            </html>
+        ';
+    }
+
+
+    private function renderIPList(){
+        $ret = "<h3>Logged Ip addresses</h3>
+
+				<ul>";
+        foreach ($this->logItems as $ipAdresses) {
+            $ip = $ipAdresses[Self::$ip];
+            $occurrences = $ipAdresses[Self::$numberOfTimes];
+            list($date, $sec) = explode(" ", $ipAdresses[Self::$microTime]);
+            $date = date("Y-m-d H:i:s", $sec);
+            $ret .= "<li>$ip</li>";
+            $ret .= "<li>Number of times:  $occurrences</li>";
+            $ret .= "<li>Logged latest at:  $date</li>";
+            $ret .="<br>";
+        }
+        $ret .= "</ul>";
+        return $ret;
+    }
 }
-
-
-//list($usec, $sec) = explode(" ", $item->m_microTime);
-//$date = date("Y-m-d H:i:s", $sec);
